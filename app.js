42a28311900a51a675ba949a00fb6ebd183ed2c3
uma-satish-tech <tirumalaumasatish2001@gmail.com>
2023-10-16 09:26:25 +0000

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+const app = express();
+app.get("/", (request, response) => {
+  const result = addDays(new Date(), 100);
+  response.send(
+    `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`
+  );
+});
+
+app.listen(3005);
+
+module.exports = app;
